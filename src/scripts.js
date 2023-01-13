427f49a67f6fb6ee9eb9121d03ffd9ac3f87890d
Sarah Hampton <stoluba@gmail.com>
2023-01-13 16:18:53 -0500

    Create function to dispaly total spent for the year

--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,49 @@
+// This is the JavaScript entry file - your code begins here
+// Do not delete or rename this file ********
 import './css/styles.css';
-import { fetchData } from './apiCalls'
+import { fetchData } from './apiCalls';
+import Traveler from './Traveler';
 
-let travelers;
-let trips;
-let destinations;
+let welcomeTraveler = document.querySelector('#welcomeTraveler');
+let displayTotalSpent = document.querySelector('#displayTotalSpent');
+let logOutButton = document.querySelector('#logOutButton');
+let pastTrips = document.querySelector('#pastTrips');
+let futureTrips = document.querySelector('#futureTrips');
+let pendingTrips = document.querySelector('#pendingTrips');
 
+
+let travelersData;
+let tripsData;
+let destinationsData;
+let traveler 
+let currentTraveler = 44;
 
 const getFetch = () => {
   fetchData()
   .then(data => {
-      console.log('data', data)
-      travelers = data[0].travelers
-      trips = data[1].trips
-      destinations = data[2].destinations
-  })
+      console.log('data', data[0].travelers)
+      travelersData = data[0].travelers
+      tripsData = data[1].trips
+      destinationsData = data[2].destinations
+      traveler = new Traveler(travelersData, tripsData, destinationsData);
+      updateYearlySpent();
+  });
+  
 }
 
-window.addEventListener('load', getFetch)
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+window.addEventListener('load', getFetch);
 
-// An example of how you tell webpack to use a CSS (SCSS) file
+
+function displayTravelersPage(travelerId) {
+  traveler.findTraveler(travelerId);
+  
+
+};
+
+function updateYearlySpent() {
+  displayTotalSpent.innerText = `You have invested $${traveler.calculateYearlyExpense(currentTraveler, '2020')} in travel for 2020!`
+  
+}
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
